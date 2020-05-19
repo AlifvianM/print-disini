be7fe2cf3ce35632c3d7facfa91701ba720fb9a3
AlifvianM <alifvianm@gmail.com>
2020-05-19 13:23:04 +0700

    3

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from .models import Pemesanan, CheckOut
 
 class PemesananForm(forms.ModelForm):
+    waktu_pengambilan = forms.DateField(
+            widget = forms.TextInput(
+                    attrs = {'type':'date'}
+                )
+        )
     class Meta:
         model = Pemesanan
         fields = (
